37e9537d15df1ed7870408258ad2946fca057e9f
Yennifer Santos <46519584+ydaponte@users.noreply.github.com>
2024-12-16 13:50:43 +0100

    Update CreateADFMSIUser.sql

--- e2e_samples/parking_sensors/sql/ddo_azuresqldw_dw/ddo_azuresqldw_dw/Security/CreateADFMSIUser.sql
+++ e2e_samples/parking_sensors/sql/ddo_azuresqldw_dw/ddo_azuresqldw_dw/Security/CreateADFMSIUser.sql
@@ -1,5 +1,5 @@
-CREATE USER '$(ADFMSI)' FROM EXTERNAL PROVIDER;
+CREATE USER mdwdops-adf-dev-ys900 FROM EXTERNAL PROVIDER;
 GO
 
-EXEC sp_addrolemember db_owner,'$(ADFMSI)';
+EXEC sp_addrolemember db_owner, mdwdops-adf-dev-ys900;
 GO
